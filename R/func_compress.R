8073ae45169bd7509daada8db36d9d4ee2c5b377
Aditya Sharma <adityashrm21@gmail.com>
2019-03-01 16:41:12 -0800

    changing compress function structure + adding out_path

--- R/func_compress.R
+++ R/func_compress.R
@@ -3,14 +3,42 @@
 #' Compress the size an image by reducing the number of bits
 #' used to represent each pixel to b
 #' 
-#' @param img array
-#' @param b integer
+#' @param img_path String , file path of the image
+#' @param b Integer, desired byte size
+#' @param out_path String, a valid output path depending on the OS
 #'
-#' @return array
+#' @return String, compressed image path
 #' @export
 #'
 #' @examples
-#' compress(img,4)
-compress <- function(img, b) {
+#' compress(img_path, 5L, paste(normalizePath(dirname(img_path)), "/abc.png", sep=""))
 
+library(R.utils)
+library(png)
+library(OpenImageR)
+library(reticulate)
+library(assertthat)
+
+compress <- function(img_path, b, out_path) {
+  
+  min_size_img <- compression(img_path, 1L, 
+                              paste(normalizePath(dirname(img_path)), "\\min.png", sep=""))
+  desired_size_img <- compression(img_path, b, 
+                                  paste(normalizePath(dirname(img_path)), "\\des.png", sep=""))
+  
+  if (image_size(min_size_img) > image_size(img_path)) {
+    file.remove(min_size_img)
+    file.remove(desired_size_img)
+    stop("Can't compress further.")
+  }
+  else if (image_size(desired_size_img) > image_size(img_path)){
+    file.remove(min_size_img)
+    file.remove(desired_size_img)
+    stop("Choose a smaller.")
+  }
+  else {
+    file.remove(min_size_img)
+    file.remove(desired_size_img)
+    return(compression(img_path, b, out_path))
+  }
 }
