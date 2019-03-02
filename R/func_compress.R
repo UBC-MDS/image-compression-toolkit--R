c0abc6406e3a0c7a8aa201823ad515247c565e08
Aditya Sharma <adityashrm21@gmail.com>
2019-03-01 16:52:01 -0800

    removed libraries from function

--- R/func_compress.R
+++ R/func_compress.R
@@ -13,12 +13,6 @@
 #' @examples
 #' compress(img_path, 5L, paste(normalizePath(dirname(img_path)), "/abc.png", sep=""))
 
-library(R.utils)
-library(png)
-library(OpenImageR)
-library(reticulate)
-library(assertthat)
-
 compress <- function(img_path, b, out_path) {
   
   min_size_img <- compression(img_path, 1L, 
